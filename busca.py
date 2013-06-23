5a3d8b23cc72739669994f866eaaf506dade18a5
roberto <roberto_sampaio@id.uff.br>
2013-06-23 11:30:23 -0300

    Atualizacao da busca

--- busca.py
+++ busca.py
@@ -1,8 +1,23 @@
 import urllib2
 import BeautifulSoup
 
-request = urllib2.Request("http://www.google.com.br")
-response = urllib2.urlopen(request)
-soup = BeautifulSoup.BeautifulSoup(response)
-for anchors in soup.findAll('a'):
-    print anchors['href']
+links = []
+limite = 1
+
+def buscaProfundidadeIterativa(url1, url2):
+
+    global limite
+
+    if url1 == url2:
+        return 
+
+    if limite == 1
+	return
+
+    requisicao = urllib2.Request(url1)
+    resposta = urllib2.urlopen(requisicao)
+    soup = BeautifulSoup.BeautifulSoup(resposta)
+    for anchors in soup.findAll('a'):
+        links << anchors['href']
+
+
